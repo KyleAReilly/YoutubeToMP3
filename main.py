110e346af0a09bebb6c688db67832814be6224d2
Kyle Reilly <Kyle.A.Reilly01@gmail.com>
2022-03-20 12:44:41 -0400

    YoutubeToMP3 Added playlists and looping

--- main.py
+++ main.py
@@ -1,20 +1,54 @@
 from email.mime import base
 from pytube import YouTube
+from pytube import Playlist
 import os
 print("\nYouTube To MP3 Format\n")
 
-URL = input("Enter Video URL:")
-yt = YouTube(URL)
+PlaylistBool = input('Is this a playlist? Y or N:').lower()
 
-try:
-    print("\nFiring Missiles")
-    video = yt.streams.filter(only_audio=True).first()
-    out_file = video.download()
 
-    base, ext = os.path.splitext(out_file)
-    new_file = base + ".mp3"
-    os.rename(out_file, new_file)
-    print("\nSuccessfully Downloaded\n")
+def videoDownload():
+    if PlaylistBool == 'n':
 
-except:
-    print("\nOPE, try again that was wonky")
+        URL = input("Enter Video URL:")
+        yt = YouTube(URL)
+        try:
+            print("\nFiring Missiles")
+            video = yt.streams.filter(only_audio=True).first()
+            out_file = video.download()
+
+            base, ext = os.path.splitext(out_file)
+            new_file = base + ".mp3"
+            os.rename(out_file, new_file)
+            print("\nSuccessfully Downloaded\n")
+
+        except:
+            print("\nOPE, try again that was wonky")
+
+    elif PlaylistBool == 'y':
+        URL_ = input("Enter Playlist URL:")
+        p = Playlist(URL_)
+
+        try:
+            print(f'Downloading: {p.title}')
+            for video in p.videos:
+                salad = video.streams.filter(only_audio=True).first()
+                out_file = salad.download()
+
+                base, ext = os.path.splitext(out_file)
+                new_file = base + ".mp3"
+                os.rename(out_file, new_file)
+
+            print("\nFiring Missiles")
+
+            print("\nSuccessfully Downloaded\n")
+
+        except:
+            print("\nOPE, try again that was wacky")
+
+    loop = input('Would you like to keep converting? Y or N').lower()
+    if loop == 'y':
+        videoDownload()
+
+
+videoDownload()
